82e3e91c63565bfc20c1f22fdc5cfc89cbb17ab6
Benjamin <bnjackson951@gmail.com>
2023-11-30 12:33:34 +0000

    Add event listeners to grid to listen for when mouse is held down and dragged across grid

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const grid = document.getElementById('grid');
 const gridRange = document.getElementById('rangeInput');
 const rangeLabel = document.getElementById('rangeLabel');
 
+// Variable to track mouse state. Value will be true when mouse held down and over the grid
+let isMouseDown = false;
+
 gridRange.addEventListener('input', () => {
     createGrid(rangeInput.value);
 });
@@ -17,10 +20,37 @@ function createGrid(rangeInput) {
     }
     grid.style.gridTemplateColumns = `repeat(${rangeInput}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${rangeInput}, ifr)`;
+    addEventHandlers();
 }
 
 function updateRangeLabel() {
     rangeLabel.innerText = `${rangeInput.value} x ${rangeInput.value}`;
+}
+
+function addEventHandlers() {
+    // Add event listeners to listen for when the mouse is dragged over the grid, and the state of the mouse 
+    grid.addEventListener('mousedown', handleMouseDown);
+    grid.addEventListener('mouseup', handleMouseUp);
+    grid.addEventListener('mouseover', handleMouseOver);
+}
+
+function handleMouseDown() {
+    isMouseDown = true;
+}
+
+function handleMouseUp() {
+    isMouseDown = false;
+}
+
+function handleMouseOver(event) {
+    console.log(event);
+    if (isMouseDown) {
+        changeColor(event.target)
+    }
+}
+
+function changeColor(item) {
+    
 }
 
 createGrid(16);
